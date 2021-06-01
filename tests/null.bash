6c2433fc8d86a6d05b71b0e59e6db6e867279865
David Farrell <davidnmfarrell@gmail.com>
2021-05-31 21:44:34 -0400

    fix test to work with token-based parse

--- tests/null.bash
+++ tests/null.bash
@@ -3,19 +3,17 @@ source "test-bootstrap.bash"
 
 function test_good_token {
   jp.trace "test_good_token"
-  JP_BUF=(n u l l)
-  JP_IDX=0
+  JP_BUF=("null")
   jp.chomp
   jp.null
-  ok $(( $JP_R == "null" )) "parse 'null' token"
+  ok $(( "${JP_TOKENS[0]}" == "null" )) "parse 'null' token"
 }
 
 function test_bad_token {
   jp.trace "test_bad_token"
-  JP_BUF=(n u l)
-  JP_IDX=0
+  JP_BUF=("null")
   jp.chomp
-  jp.null
+  jp.null 2> /dev/null # silence the error msg
   ok $(( $? == 1 )) "parse 'nul' is an error"
 }
 
