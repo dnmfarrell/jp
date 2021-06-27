057c05e6057762c31b349bdcdba6b3d12bcf7424
David Farrell <davidnmfarrell@gmail.com>
2021-06-26 20:45:03 -0400

    jp might not be in PATH

--- tests/halts.bash
+++ tests/halts.bash
@@ -3,8 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/../test-bootstrap.bash"
 
 function test_should_halt {
-    cat "$DIR/share/ec2-describe-instances.json" | jp -p
-      ok $(( $? == 0 )) "got here"
+  cat "$DIR/share/ec2-describe-instances.json" | "$DIR/../jp" -p
+  ok $(( $? == 0 )) "jp didn't halt on large input"
 }
 
 test_should_halt
