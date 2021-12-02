9fff4481e8ed946dcb618bf1c0ebb20e5cbcda65
David Farrell <1469333+dnmfarrell@users.noreply.github.com>
2021-12-02 17:28:09 -0500

    .not complements bools

---
+++ tests/transform/not.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+source "test-bootstrap.bash"
+IFS=
+
+$(./jp .not 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "not empty stack errors"
+else
+  fail "not empty stack does not error"
+fi
+
+$(./jp null .not 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "not illegal operand type errors"
+else
+  fail "not illegal operand type does not error"
+fi
+
+t=$(./jp 'true' .not)
+if [ "$t" = 'false' ];then
+  pass "true not returns false"
+else
+  printf -v tesc "%q" "$t"
+  fail "true not returns: $tesc"
+fi
+
+f=$(./jp 'false' .not)
+if [ "$f" = 'true' ];then
+  pass "false not returns true"
+else
+  printf -v fesc "%q" "$f"
+  fail "false not returns: $fesc"
+fi
