ba053cd4d7acc01995bf5dcc902cd1f16e9e474f
David Farrell <1469333+dnmfarrell@users.noreply.github.com>
2021-12-06 22:02:15 -0500

    and/or functions

---
+++ tests/transform/and.bash
@@ -0,0 +1,63 @@
+#!/bin/bash
+source "test-bootstrap.bash"
+IFS=
+
+$(./jp .and 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "and empty stack errors"
+else
+  fail "and empty stack does not error"
+fi
+
+$(./jp true .and 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "and missing right operand errors"
+else
+  fail "and missing right operand does not error"
+fi
+
+$(./jp null .and 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "and illegal left operand errors"
+else
+  fail "and illegal left operand does not error"
+fi
+
+$(./jp false null .and 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "and illegal right operand errors"
+else
+  fail "and illegal right operand does not error"
+fi
+
+tt=$(./jp true true .and)
+if [ "$tt" = 'true' ];then
+  pass "and true true returns true"
+else
+  printf -v ttesc "%q" "$tt"
+  fail "and true true returns: $ttesc"
+fi
+
+tf=$(./jp true false .and)
+if [ "$tf" = 'false' ];then
+  pass "and true false returns false"
+else
+  printf -v tfesc "%q" "$tf"
+  fail "and true false returns: $tfesc"
+fi
+
+ft=$(./jp false true .and)
+if [ "$ft" = 'false' ];then
+  pass "and false true returns false"
+else
+  printf -v ftesc "%q" "$ft"
+  fail "and false true returns: $ftesc"
+fi
+
+ff=$(./jp false false .and)
+if [ "$ff" = 'false' ];then
+  pass "and false false returns false"
+else
+  printf -v ffesc "%q" "$ff"
+  fail "and false false returns: $ffesc"
+fi
---
+++ tests/transform/or.bash
@@ -0,0 +1,63 @@
+#!/bin/bash
+source "test-bootstrap.bash"
+IFS=
+
+$(./jp .or 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "or empty stack errors"
+else
+  fail "or empty stack does not error"
+fi
+
+$(./jp true .or 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "or missing right operand errors"
+else
+  fail "or missing right operand does not error"
+fi
+
+$(./jp null .or 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "or illegal left operand errors"
+else
+  fail "or illegal left operand does not error"
+fi
+
+$(./jp false null .or 2>/dev/null)
+if [ $? -eq 1 ];then
+  pass "or illegal right operand errors"
+else
+  fail "or illegal right operand does not error"
+fi
+
+tt=$(./jp true true .or)
+if [ "$tt" = 'true' ];then
+  pass "or true true returns true"
+else
+  printf -v ttesc "%q" "$tt"
+  fail "or true true returns: $ttesc"
+fi
+
+tf=$(./jp true false .or)
+if [ "$tf" = 'true' ];then
+  pass "or true false returns true"
+else
+  printf -v tfesc "%q" "$tf"
+  fail "or true false returns: $tfesc"
+fi
+
+ft=$(./jp false true .or)
+if [ "$ft" = 'true' ];then
+  pass "or false true returns true"
+else
+  printf -v ftesc "%q" "$ft"
+  fail "or false true returns: $ftesc"
+fi
+
+ff=$(./jp false false .or)
+if [ "$ff" = 'false' ];then
+  pass "or false false returns false"
+else
+  printf -v ffesc "%q" "$ff"
+  fail "or false false returns: $ffesc"
+fi
